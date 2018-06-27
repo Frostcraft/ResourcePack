08903ae74549d8ae42198a26b7bf1d0c0cf629e5
FrostMiser <FrostMiser@users.noreply.github.com>
2018-06-26 21:09:42 -0500

    Add spacing and warn if temporary path is found

--- build_resource_pack.py
+++ build_resource_pack.py
@@ -21,8 +21,11 @@ def setup_logging():
 def create_temporary_copy(path):
     temp_dir = tempfile.gettempdir()
     temp_path = os.path.join(temp_dir, "fc_build")
+    if os.path.exists(temp_path):
+        logging.warn("Temporary path already exists at {}".format(temp_path))
     shutil.copytree(path, temp_path)
     return temp_path
+
 
 def clean_items(where):
     def check(arr):
@@ -37,13 +40,14 @@ def clean_items(where):
         check(dirs)
         check(files)
 
+
 def create_resource_pack():
     temp_path = create_temporary_copy("./src")
     logging.debug("Building resource pack, using temp path: {}".format(temp_path))
     clean_items(temp_path)
     shutil.make_archive('froscraft-resource-pack', 'zip', temp_path)
     shutil.rmtree(temp_path)
-   
+
 
 if __name__ == "__main__":
     setup_logging()
